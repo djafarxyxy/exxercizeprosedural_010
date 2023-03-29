cdd91f50161c15c068d51b002aa17a55e61800b3
djafarxyxy <farhanch12344@gmail.com>
2023-03-29 14:48:05 +0700

    exer

--- exxercizeprosedural_010/exxercizeprosedural_010.cpp
+++ exxercizeprosedural_010/exxercizeprosedural_010.cpp
@@ -1,20 +1,46 @@
-// exxercizeprosedural_010.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+using namespace std;
 
-int main()
-{
-    std::cout << "Hello World!\n";
+double rerata(double a, double b) {
+	return(a + b) /2;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+string status(double r) {
+	if (r >= 70) {
+		return "lulus";
+	}else {
+		return "gagal";
+	}
+}
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+string status2(double r, double n) {
+	if (r >= 70 && n >= 80) {
+		return "lulus";
+	}
+	else {
+	}
+}
+
+string status3(double r, double n) {
+	if (r >= 70 || n >= 70) {
+		return "lulus";
+	}
+	else {
+		return "gagal";
+	}
+}
+
+int main() {
+	double nilaimath, nilaiBing;
+
+
+	cout << "Masukan nilai matematika : ";
+	cin >> nilaimath;
+	cout << "masukan nilai B.inggris : ";
+	cin >> nilaiBing;
+
+	cout << "nilai Rerata : " << rerata(nilaimath, nilaiBing);
+	cout << "\nstatus kelulusannya : " << status(rerata(nilaimath, nilaiBing));
+	cout << "\nstatus kelulusannya : " << status2(rerata(nilaimath, nilaiBing), nilaimath);
+	cout << "\nstatus kelulusannya : " << status3(rerata(nilaimath, nilaiBing), nilaimath);
+}
